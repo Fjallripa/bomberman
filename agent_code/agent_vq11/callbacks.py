96412274346f321c4b8671f6c17e021d03013d92
Fjallripa <58792390+Fjallripa@users.noreply.github.com>
2022-03-11 13:40:25 +0100

    preparation for agent_vq11

--- agent_code/agent_vq11/callbacks.py
+++ agent_code/agent_vq11/callbacks.py
@@ -1,16 +1,24 @@
+# Callbacks for agent_vq11
+# ========================
+
+
 import os
 import pickle
 import random
-
 import numpy as np
 
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 
-model_file = "model_vq10ch1.pt"
+model_file = "model_vq11ch1.pt"
 epsilon = 0.5   # constant for now, could be a function depending on training round later
 
 
+
+
+
+# Main functions
+# --------------
 
 def setup(self):
     """
@@ -28,7 +36,6 @@ def setup(self):
     """
 
     
-
     if self.train:
         # Setup done in setup_training()
         pass
@@ -73,7 +80,10 @@ def act(self, game_state: dict) -> str:
 
 
 
-# Support functions for act()
+
+
+# Support functions
+# -----------------
 
 def state_to_features(game_state: dict) -> np.array:
     """
@@ -89,6 +99,8 @@ def state_to_features(game_state: dict) -> np.array:
     :param game_state:  A dictionary describing the current game board.
     :return: np.array
     """
+    
+    
     # This is the dict before the game begins and after it ends
     if game_state is None:
         return None
@@ -117,8 +129,10 @@ def state_to_features(game_state: dict) -> np.array:
     return(X) 
 
 
+
 def look_for_targets(free_space, start, targets, logger=None):
-    """Find direction of closest target that can be reached via free tiles.
+    """
+    Find direction of closest target that can be reached via free tiles.
 
     Performs a breadth-first search of the reachable free tiles until a target is encountered.
     If no target can be reached, the path that takes the agent closest to any target is chosen.
@@ -131,6 +145,8 @@ def look_for_targets(free_space, start, targets, logger=None):
     Returns:
         coordinate of first step towards closest target or towards tile closest to any target.
     """
+    
+    
     if len(targets) == 0: return None
 
     frontier = [start]
@@ -141,6 +157,7 @@ def look_for_targets(free_space, start, targets, logger=None):
 
     while len(frontier) > 0:
         current = frontier.pop(0)
+        
         # Find distance from current position to all targets, track closest
         d = np.sum(np.abs(np.subtract(targets, current)), axis=1).min()
         if d + dist_so_far[current] <= best_dist:
@@ -150,6 +167,7 @@ def look_for_targets(free_space, start, targets, logger=None):
             # Found path to a target's exact position, mission accomplished!
             best = current
             break
+        
         # Add unexplored free neighboring tiles to the queue in a random order
         x, y = current
         neighbors = [(x, y) for (x, y) in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)] if free_space[x, y]]
@@ -159,7 +177,9 @@ def look_for_targets(free_space, start, targets, logger=None):
                 frontier.append(neighbor)
                 parent_dict[neighbor] = current
                 dist_so_far[neighbor] = dist_so_far[current] + 1
+    
     if logger: logger.debug(f'Suitable target found at {best}')
+    
     # Determine the first step towards the best found target tile
     current = best
     while True:
@@ -167,63 +187,26 @@ def look_for_targets(free_space, start, targets, logger=None):
         current = parent_dict[current]
 
 
-def state_to_features(game_state: dict) -> np.array:
-    """
-    *This is not a required function, but an idea to structure your code.*
-
-    Converts the game state to the input of your model, i.e.
-    a feature vector.
-
-    You can find out about the state of the game environment via game_state,
-    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
-    what it contains.
-
-    :param game_state:  A dictionary describing the current game board.
-    :return: np.array
-    """
-    # This is the dict before the game begins and after it ends
-    if game_state is None:
-        return None
-
-    ### design features for Task 1 ###
-    """
-    np.array where each component corresponds to on neighbour field
-    = 0 if wall or crate
-    = 1 if free
-    = 2 if free and (one) nearest field to nearest coin
-    """
-    X = np.zeros(4, dtype = int) # hand-crafted feature vector
-    
-    free_space = game_state['field'] == 0 # Boolean numpy array. True for free tiles and False for Crates & Walls
-    agent_x, agent_y = game_state['self'][3] # Agent position as coordinates 
-    coin_direction = look_for_targets(free_space, (agent_x, agent_y), game_state['coins']) # neighbouring field closest to closest coin
-
-    neighbours = [(agent_x + 1, agent_y), (agent_x - 1, agent_y), (agent_x, agent_y + 1), (agent_x, agent_y - 1)]
-    
-    for j, neighbour in enumerate(neighbours):
-        if neighbour == coin_direction: 
-            X[j] = 2
-        elif free_space[neighbour[0], neighbour[1]]:
-            X[j] = 1
-    
-    return(X) 
-
 
 def epsilon_greedy (recommended_action, epsilon):
-    random_action = lambda x: np.random.choice(ACTIONS[:4])  # don't kill yourself, maybe only compute if needed?
+    
+    random_action = lambda x: np.random.choice(ACTIONS[:4])  # don't kill yourself
 
     return np.random.choice([recommended_action, random_action()], p = [1 - epsilon, epsilon])
 
 
-def find_state (features):
-    # currently just assigns one state to each unique features
-    # currently, there are 3^4 = 81 different states
-    # every point in the feature space gets assigned one number
 
-    #print(features)
+def find_state (features):
+    """
+    currently just assigns one state to each unique features
+    currently, there are 3^4 = 81 different states
+    every point in the feature space gets assigned one number
+    """
+    
+    
     state_index = features[0]*3**3 + features[1]*3**2 + features[2]*3 + features[3]
-    #print(state_index)
     return state_index  
+
 
 
 def random_argmax_1d(a):
@@ -245,19 +228,3 @@ def random_argmax_1d(a):
     
     max_indices = np.where(a == np.max(a))[0]
     return np.random.choice(max_indices)
-
-    '''
-    maximum = np.amax(a) 
-    smaller_than_max = maximum - 1
-
-    if np.count_nonzero(a == maximum) == 1: 
-        return(np.argmax(a))
-    else:
-        argmaxima = []
-        b = a
-        while(np.amax(b) == maximum):
-            i = np.argmax(b)
-            argmaxima.append(i)
-            b[i] = smaller_than_max
-        return(np.random.choice(np.array(argmaxima)))
-    '''
