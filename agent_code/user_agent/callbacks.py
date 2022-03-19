5004dbcf7a3f948e5f6e85b354062f57526449fa
Fjallripa <58792390+Fjallripa@users.noreply.github.com>
2022-03-19 15:43:30 +0100

    prototyped functions to compute best bombing spot

--- agent_code/user_agent/callbacks.py
+++ agent_code/user_agent/callbacks.py
@@ -4,4 +4,5 @@ def setup(self):
 
 def act(self, game_state: dict):
     self.logger.info('Pick action according to pressed key')
+    print(game_state['field'])
     return game_state['user_input']
