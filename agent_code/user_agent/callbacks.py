b5d0f3650f15453302cc97cde39bea10b22686dd
Fjallripa <58792390+Fjallripa@users.noreply.github.com>
2022-03-19 19:03:06 +0100

    Moved on to test the good_spot function

--- agent_code/user_agent/callbacks.py
+++ agent_code/user_agent/callbacks.py
@@ -4,5 +4,5 @@ def setup(self):
 
 def act(self, game_state: dict):
     self.logger.info('Pick action according to pressed key')
-    print(game_state['field'])
+    print(repr(game_state['field']))
     return game_state['user_input']
