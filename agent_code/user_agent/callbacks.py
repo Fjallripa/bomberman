42fbe551c9b1f042de62eaab8943cc8779436ac6
Fjallripa <58792390+Fjallripa@users.noreply.github.com>
2022-03-21 17:14:49 +0100

    Testing of coin goal functions complete

--- agent_code/user_agent/callbacks.py
+++ agent_code/user_agent/callbacks.py
@@ -4,5 +4,4 @@ def setup(self):
 
 def act(self, game_state: dict):
     self.logger.info('Pick action according to pressed key')
-    print(repr(game_state['field']))
     return game_state['user_input']
